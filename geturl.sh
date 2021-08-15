1427504fe0c4dcee62dc39f8d39e5e6e0d97d0e5
ARAVIND <dummymail@gmail.com>
2021-08-15 20:18:10 +0530

    geturl.sh modified

--- geturl.sh
+++ geturl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd /opt/docker/compose
+wget https://li.nux.ro/download/nux/dextop/el7/x86_64/html2text-1.3.2a-14.el7.nux.x86_64.rpm
+sudo rpm -Uvh html2text-1.3.2a-14.el7.nux.x86_64.rpm
+
 baseurl=$(echo http://15.207.109.98:8081/repository) ;
 repo=$(curl curl http://15.207.109.98:8081/service/rest/repository/browse/ekams/ | html2text | tail -n1 | cut -b 1-5) ;
 groupid=$(curl curl http://15.207.109.98:8081/service/rest/repository/browse/ekams/ | html2text | tail -n1 | cut -b 1-5) ;
