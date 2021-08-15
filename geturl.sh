af2df8920f713ab9f437542cbaa0f7eb3f085b6a
ARAVIND <dummymail@gmail.com>
2021-08-15 20:03:18 +0530

    nexus ip changed

--- geturl.sh
+++ geturl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-baseurl=$(echo http://3.108.236.14:8081/repository) ;
-repo=$(curl curl http://3.108.236.14:8081/service/rest/repository/browse/ekams/ | html2text | tail -n1 | cut -b 1-5) ;
-groupid=$(curl curl http://3.108.236.14:8081/service/rest/repository/browse/ekams/ | html2text | tail -n1 | cut -b 1-5) ;
-artifactid=$(curl http://3.108.236.14:8081/service/rest/repository/browse/ekams/ekams/ | html2text | tail -n1 | cut -b 1-9) ;
-snapshotversion=$(curl http://3.108.236.14:8081/service/rest/repository/browse/ekams/ekams/ekams-web/ | html2text | grep -v metadata | tail -n1) ;
-snapshotid=$(curl http://3.108.236.14:8081/service/rest/repository/browse/ekams/ekams/ekams-web/1-SNAPSHOT/ | html2text | grep -v metadata | tail -n1) ;
+baseurl=$(echo http://15.207.109.98:8081/repository) ;
+repo=$(curl curl http://15.207.109.98:8081/service/rest/repository/browse/ekams/ | html2text | tail -n1 | cut -b 1-5) ;
+groupid=$(curl curl http://15.207.109.98:8081/service/rest/repository/browse/ekams/ | html2text | tail -n1 | cut -b 1-5) ;
+artifactid=$(curl http://15.207.109.98:8081/service/rest/repository/browse/ekams/ekams/ | html2text | tail -n1 | cut -b 1-9) ;
+snapshotversion=$(curl http://15.207.109.98:8081/service/rest/repository/browse/ekams/ekams/ekams-web/ | html2text | grep -v metadata | tail -n1) ;
+snapshotid=$(curl http://15.207.109.98:8081/service/rest/repository/browse/ekams/ekams/ekams-web/1-SNAPSHOT/ | html2text | grep -v metadata | tail -n1) ;
 
 url=$(echo $baseurl/$repo/$groupid/$artifactid/$snapshotversion/$artifactid-$snapshotid.war)
 
